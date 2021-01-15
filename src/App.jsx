5b9321b5ef09aee97f5754d4d8f7109808c5f182
HarryHoudini <dev.jon.jonson@gmail.com>
2021-01-16 00:43:34 +0300

    Add first icons

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './components/home/Home'
+import Header from './components/header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hey clever Programmer Let's build!</h1> */}
+      <Header></Header>
+
+      <Home>
+      </Home>
+
+      {/* Home */}
+      {/* Header */}
+      {/* Banner */}
+      {/* Search */}
+      {/* Cards */}
+      {/* SearchPage */}
+      {/* Header */}
+      {/* ... */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Avatar} from '@material-ui/core';
+import LanguageIcon from '@material-ui/icons/Language';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header__icon' src='https://i.pinimg.com/originals/cf/9d/1a/cf9d1a566053275b76ddc464f6dc24c8.png' alt='' />
+
+            <div className='header__center'>
+                <input type="text"/>
+                <SearchIcon/>
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+                <LanguageIcon></LanguageIcon>
+                <ExpandMoreIcon></ExpandMoreIcon>
+                <Avatar/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
