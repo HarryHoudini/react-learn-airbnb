49c43185191b418699d8cd91ae92944f6fcfdfa8
HarryHoudini <dev.jon.jonson@gmail.com>
2021-01-23 22:20:50 +0300

    Add link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {Avatar} from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
 import LanguageIcon from '@material-ui/icons/Language';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className='header'>
-            <img className='header__icon' src='https://i.pinimg.com/originals/cf/9d/1a/cf9d1a566053275b76ddc464f6dc24c8.png' alt='' />
+            <Link to='/'>
+                <img className='header__icon' src='https://i.pinimg.com/originals/cf/9d/1a/cf9d1a566053275b76ddc464f6dc24c8.png' alt='' />
+
+            </Link>
 
             <div className='header__center'>
-                <input type="text"/>
-                <SearchIcon/>
+                <input type="text" />
+                <SearchIcon />
 
             </div>
 
@@ -20,7 +24,7 @@ function Header() {
                 <p>Become a host</p>
                 <LanguageIcon></LanguageIcon>
                 <ExpandMoreIcon></ExpandMoreIcon>
-                <Avatar/>
+                <Avatar />
 
             </div>
         </div>
